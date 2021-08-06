2d286a4b92d3ae5d766824082b0d8e58e7d39f33
Fikir <fikirmuluken@gmail.com>
2021-08-06 22:03:49 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const app = express();
 const port = 2022;
 
 
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json);
 
-app.post('/register', (req, res) => {});
+app.post('/register', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Registration was successful",
+    });
+});
 
-app.listen(port);
-console.log(`Server running on port: ${port}`);
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
